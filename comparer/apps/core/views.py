086bcd1dfa8f410e6bc98934387b272a52b49229
Yuri Kriachko <yuriykryachko@gmail.com>
2015-09-21 20:38:35 +0300

    can calc ahash

--- comparer/apps/core/views.py
+++ comparer/apps/core/views.py
@@ -5,9 +5,27 @@ from django.contrib import messages
 from django.views.generic.edit import FormView
 
 from core.forms import ComparerForm
+from core.hash_utils import Hash
 
 
 class HomeView(FormView):
     form_class = ComparerForm
     template_name = "home.html"
     success_url = '/'
+
+    def form_valid(self, form):
+        data = self.get_context_data() 
+        data['form'] = form
+
+        first_image = form.cleaned_data['first_image']
+        second_image = form.cleaned_data['second_image']
+
+        data['first_image'] = first_image
+        data['second_image'] = second_image
+
+        data['first_image_score'] = Hash(first_image).ahash()
+        data['second_image_score'] = Hash(second_image).ahash()
+
+        data['diff_score'] = 1 
+
+        return self.render_to_response(data)
